3d2bc8b614262c7f4331146e99e0f4faa52d8351
Cristina Munoz <hi@xmunoz.com>
2016-12-04 22:55:09 +0100

    foo

--- test/test.jl
+++ test/test.jl
@@ -1,3 +1,3 @@
 using Molcajete
 
-Molcajete.show_calendar("Julia-Users-Group", Date(2016, 12, 1), Date(2016, 12, 31), num_meetups = 20)
+Molcajete.show_calendar("Julia-Users-Group", Date(2016, 12, 1), Date(2016, 12, 31), num_meetups=20)
